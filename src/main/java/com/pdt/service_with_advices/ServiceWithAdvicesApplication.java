e6bf8698672f915a77c17ccf41ac736acd0f22e4
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-08-06 00:05:01 +0200

    st

---
+++ src/main/java/com/pdt/service_with_advices/ServiceWithAdvicesApplication.java
@@ -0,0 +1,13 @@
+package com.pdt.service_with_advices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWithAdvicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceWithAdvicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdt/service_with_advices/ServiceWithAdvicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdt.service_with_advices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceWithAdvicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
